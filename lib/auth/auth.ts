10179c7f4848dd5b9ef1363b31fd00d1c2a748a3
Balasarathi29 <bala2004sarathi@gmail.com>
2026-03-23 19:19:42 +0530

    Auto-create job board on signup

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,6 +3,8 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { initializeUserBoard } from "../init-user-board";
+import { userAc } from "better-auth/plugins/admin/access";
 
 
 const uri = process.env.MONGODB_URI;
@@ -18,6 +20,17 @@ export const auth = betterAuth({
   }),
   emailAndPassword :{
     enabled : true,
+  },
+  databaseHooks : {
+    user:{
+      create : {
+        after : async (user) =>{
+          if(user.id){
+            await initializeUserBoard(user.id)
+          }
+        }
+      }
+    }
   }
 });
 
