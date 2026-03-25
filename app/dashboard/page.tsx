c1946f83f2c7075f6ddc4fcc27e77f392fb7479e
Balasarathi29 <bala2004sarathi@gmail.com>
2026-03-25 10:48:44 +0530

    Add Kanban board scaffold and board models

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { getSession } from "@/lib/auth/auth"
 import connectDB from "@/lib/db"
 import { Board } from "@/lib/models"
 import { redirect } from "next/navigation"
+import KanbanBoard from "@/components/kanbanBoard"
+
 
 
 export default async function Dashboard() {
@@ -17,5 +19,14 @@ export default async function Dashboard() {
      })
      console.log(board)
 
-    return <div>Welcome this is Dashboard</div>
+    return 
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto p-6">
+        <div className="mb-6">
+          <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
+          <p className="text-gray-600">Track your job applications</p>
+        </div>
+        <KanbanBoard board={board} userId={session.user.id} />
+      </div>
+    </div>
 }
---
+++ components/kanbanBoard.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Board } from "@/lib/models/models.type";
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+
+interface kanbanBoardProps {
+    board : Board;
+    userId : string;
+}
+
+interface ColConfig {
+  color: string;
+  icon: React.ReactNode;
+}
+
+const COLUMN_CONFIG: Array<ColConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />,
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+];
+
+
+
+
+
+export default function KanbanBoard({board , userId} : kanbanBoardProps) {
+    const columns = board.columns
+    return 
+    <>
+        <div>
+            <div>
+                {columns.map((col, key) => {
+                    const config = COLUMN_CONFIG[key] || {
+                        color: "bg-gray-500",
+                        icon: <Calendar className="h-4 w-4" />,
+                    };
+                    return (
+                        <>
+                        </>
+                )})}
+            </div>
+        </div>
+    </>
+}
